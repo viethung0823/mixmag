b5546adbbc3baaeecbee2b0e17d7daf23065ae74
viethung <viethung0823@gmail.com>
2023-06-18 10:44:08 +0700

    asdfadsf

--- src/SP.js
+++ src/SP.js
@@ -5,7 +5,7 @@ window.onload = function () {
 		document.querySelectorAll(".blog-basic-grid--container div")[index]?.appendChild(metaSection);
 	});
 
-	const metaSection1 = document.querySelectorAll(".summary-metadata-container summary-metadata-container--above-title");
+	const metaSection1 = document.querySelectorAll(".summary-metadata-container.summary-metadata-container--above-title");
 	metaSection1.forEach((metaSection, index) => {
 		metaSection?.classList.add("context-marker");
 		document.querySelectorAll(".summary-thumbnail-outer-container")[index]?.appendChild(metaSection);
