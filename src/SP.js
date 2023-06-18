faa9eeac16997564f28cdd88acac31d11060aa7c
viethung <viethung0823@gmail.com>
2023-06-18 10:25:52 +0700

    build

--- src/SP.js
+++ src/SP.js
@@ -1,9 +1,13 @@
 window.onload = function () {
-	const metaSection = document.querySelector(".blog-meta-section");
-	metaSection?.classList.add("context-marker");
-	document.querySelector(".blog-basic-grid--container div")?.appendChild(metaSection);
+	const metaSections = document.querySelectorAll(".blog-meta-section");
+	metaSections.forEach((metaSection) => {
+		metaSection?.classList.add("context-marker");
+		document.querySelector(".blog-basic-grid--container div")?.appendChild(metaSection);
+	});
 
-	const metaSection1 = document.querySelector(".summary-metadata-container");
-	metaSection1?.classList.add("context-marker");
-	document.querySelector(".summary-thumbnail-outer-container")?.appendChild(metaSection1);
+	const metaSection1 = document.querySelectorAll(".summary-metadata-container");
+	metaSections.forEach((metaSection) => {
+		metaSection1?.classList.add("context-marker");
+		document.querySelector(".summary-thumbnail-outer-container")?.appendChild(metaSection1);
+	});
 };
