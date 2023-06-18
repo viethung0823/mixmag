16a938739449def04b00d3d1c971a1af4782c72d
viethung <viethung0823@gmail.com>
2023-06-18 09:54:51 +0700

    build

--- src/SP.js
+++ src/SP.js
@@ -2,4 +2,8 @@ window.onload = function () {
 	const metaSection = document.querySelector(".blog-meta-section");
 	metaSection?.classList.add("context-marker");
 	document.querySelector(".blog-basic-grid--container div")?.appendChild(metaSection);
+
+	const metaSection1 = document.querySelector(".summary-metadata-container");
+	metaSection1?.classList.add("context-marker");
+	document.querySelector(".summary-thumbnail-outer-container")?.appendChild(metaSection1);
 };
