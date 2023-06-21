fffd65dfe26ba8187b636840b301662ba06d80bf
viethung <viethung0823@gmail.com>
2023-06-21 23:08:51 +0700

    asdfad

--- src/SP.js
+++ src/SP.js
@@ -10,4 +10,9 @@ window.onload = function () {
 		metaSection?.classList.add("context-marker");
 		document.querySelectorAll(".summary-thumbnail-outer-container")[index]?.appendChild(metaSection);
 	});
+
+	document.querySelector("img[src='https://images.squarespace-cdn.com/content/v1/6465baf2d00ad7078486fd8f/62f156aa-6937-4dae-96f5-426fec0ad156/image-asset.jpeg?format=2500w']").src = document.querySelector(".summary-thumbnail > img").src.replace(/\?.*/, "");
+	const newHref = document.querySelector(".summary-thumbnail-outer-container a").href;
+	document.querySelector(".sqs-block-image-link.content-fill").href = newHref;
+	document.querySelector(".summary-item").remove();
 };
