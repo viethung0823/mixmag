a7e2d284aad6ebd73ab1b86454709ae2f7aeb052
viethung <viethung0823@gmail.com>
2023-06-21 23:17:38 +0700

    sfgsdf

--- src/SP.js
+++ src/SP.js
@@ -12,6 +12,9 @@ window.onload = function () {
 	});
 
 	const newImg = document.querySelector(".summary-thumbnail > img").src.replace(/\?.*/, "");
+	const newImg1 = document.querySelectorAll(".summary-thumbnail > img")[0].src.replace(/\?.*/, "");
+	console.log('🚀 ~ newImg:', newImg)
+	console.log('🚀 ~ newImg:', newImg1)
 	if (newImg) {
 		document.querySelector("img[src='https://images.squarespace-cdn.com/content/v1/6465baf2d00ad7078486fd8f/62f156aa-6937-4dae-96f5-426fec0ad156/image-asset.jpeg?format=2500w']").src = newImg;
 		const newHref = document.querySelector(".summary-thumbnail-outer-container a").href;
