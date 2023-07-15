af6fd09eb405ae7ca67f888867c81e801449d0cf
viethung <viethung0823@gmail.com>
2023-07-16 06:59:16 +0700

    asdf

--- src/SP.js
+++ src/SP.js
@@ -15,8 +15,9 @@ window.onload = function () {
 	const newImg1 = document.querySelectorAll(".summary-thumbnail > img")[0]?.getAttribute('data-src');
 	const newImg2 = document.querySelectorAll(".summary-thumbnail > img")[0]?.getAttribute('data-image');
 	const newImg3 = document.querySelectorAll(".summary-thumbnail > img")[0]?.getAttribute('src')?.split('?')?.[0];
+
 	document.querySelector("img[data-image='https://images.squarespace-cdn.com/content/v1/6465baf2d00ad7078486fd8f/62f156aa-6937-4dae-96f5-426fec0ad156/image-asset.jpeg']").src = newImg || newImg1 || newImg2 || newImg3;
-	console.log(document.querySelector("img[data-image='https://images.squarespace-cdn.com/content/v1/6465baf2d00ad7078486fd8f/62f156aa-6937-4dae-96f5-426fec0ad156/image-asset.jpeg']"))
+	document.querySelector("img[data-image='https://images.squarespace-cdn.com/content/v1/6465baf2d00ad7078486fd8f/62f156aa-6937-4dae-96f5-426fec0ad156/image-asset.jpeg']").dataset.src= newImg || newImg1 || newImg2 || newImg3;
 	const newHref = document.querySelector(".summary-thumbnail-outer-container a").href;
 	document.querySelector(".sqs-block-image-link.content-fill").href = newHref;
 	document.querySelector(".sqs-html-content h3 a strong").innerText = document.querySelector(".summary-title-link").innerText;
